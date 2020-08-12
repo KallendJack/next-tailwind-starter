fcd196aa57fcc0addcaa342839bb24680218aaff
Jack Kallend <Kallendo96@live.co.uk>
2020-08-12 15:38:39 +0100

    Initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
-import '../styles/globals.css'
+import Head from 'next/head'
+import '../css/_main.scss'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+const App = ({ Component, pageProps }) => {
+  return (
+    <>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
-export default MyApp
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: {
+    enabled: process.env.NODE_ENV === 'production' ? true : false,
+    content: ['./pages/**/*.js', './components/**/*.js'],
+  },
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+}
