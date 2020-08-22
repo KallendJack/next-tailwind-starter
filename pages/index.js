642c5cb95e2f333b356ad0fa1c93b6874c1ad77d
Jack Kallend <Kallendo96@live.co.uk>
2020-08-22 10:33:12 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -3,9 +3,7 @@ import Layout from '../components/layouts/Layout'
 const Index = () => {
   return (
     <>
-      <Layout>
-        <section></section>
-      </Layout>
+      <Layout></Layout>
     </>
   )
 }
