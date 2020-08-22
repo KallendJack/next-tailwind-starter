1a6cc012ac2b4c1b130d450f420f180472c6449f
Jack Kallend <Kallendo96@live.co.uk>
2020-08-22 10:32:12 +0100

    Removed auth0 overspill

--- components/layouts/Layout.js
+++ components/layouts/Layout.js
@@ -1,14 +1,12 @@
 import Head from 'next/head'
-import Header from '../Header'
 
-const Layout = ({ user, loading = false, children }) => {
+const Layout = ({ children }) => {
   return (
     <>
       <Head>
         <title>Next Tailwind Starter</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Header user={user} loading={loading} />
       <main>{children}</main>
     </>
   )
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,10 @@
 import Layout from '../components/layouts/Layout'
-import { useFetchUser } from '../lib/user'
 
 const Index = () => {
-  const { user, loading } = useFetchUser()
-
   return (
     <>
-      <Layout user={user} loading={loading}>
-        <section>Hello</section>
+      <Layout>
+        <section></section>
       </Layout>
     </>
   )
