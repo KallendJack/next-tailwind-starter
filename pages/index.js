627eba674b60d67ef7d50b0f0dbf2e6e60d92c4e
Jack Kallend <Kallendo96@live.co.uk>
2020-08-31 13:57:22 +0100

    NextAuth added

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,25 @@
 import Layout from '../components/layouts/Layout'
+import { signIn, signOut, useSession } from 'next-auth/client'
 
 const Index = () => {
+  const [session, loading] = useSession()
+
   return (
     <>
-      <Layout></Layout>
+      <Layout>
+        {!session && (
+          <>
+            Not signed in <br />
+            <button onClick={signIn}>Sign in</button>
+          </>
+        )}
+        {session && (
+          <>
+            Signed in as {session.user.email} <br />
+            <button onClick={signOut}>Sign out</button>
+          </>
+        )}
+      </Layout>
     </>
   )
 }
